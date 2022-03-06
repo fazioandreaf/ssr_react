f08c4da3ac0e279992a98680c92e8b013badbdd2
andrea <fazioadndrea>
2022-03-06 10:44:29 +0100

    Setup router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+
+import { Home } from "./pages/Home";
+import { Articles } from "./pages/Articles";
+import { About } from "./pages/About";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<BrowserRouter>
+				<h1>Server-Side Rendering Example</h1>
+				<ul>
+					<li>
+						<Link to='/'>Home</Link>
+					</li>
+					<li>
+						<Link to='/about'>About</Link>
+					</li>
+					<li>
+						<Link to='/articles'>Articles</Link>
+					</li>
+				</ul>
+				<Switch>
+					<Route path='/' exact>
+						<Home />
+					</Route>
+					<Route path='/about' exact>
+						<About />
+					</Route>
+					<Route path='/articles' exact>
+						<Articles />
+					</Route>
+				</Switch>
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
