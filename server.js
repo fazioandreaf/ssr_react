c39c211cd0b048ff6329806db415ca5c40a64e9c
andrea <fazioadndrea>
2022-03-06 11:53:32 +0100

    replace stati html with app component

--- server.js
+++ server.js
@@ -1,26 +1,31 @@
 import express from "express";
 import React from "react";
 import { renderToString } from "react-dom/server";
-import { Home } from "./src/pages/Home";
-
+import { StaticRouter } from "react-router-dom";
+import App from "./src/App";
+import path from "path";
+import fs from "fs";
 const app = express();
 
 app.use(express.static("./build", { index: false }));
 
 app.get("/*", (req, res) => {
-	const reactApp = renderToString(<Home />);
-
-	return res.send(
-		`
-        <html>
-            <body>
-            <div id="root">
-            ${reactApp}
-            </div>
-            </body>
-        </html>
-        `
+	const reactApp = renderToString(
+		<StaticRouter locatiion={req.url}>
+			<App />
+		</StaticRouter>
 	);
+
+	const templateFile = path.resolve("./build/index.html");
+	fs.readFile(templateFile, "utf8", (err, data) => {
+		if (err) {
+			return res.status(500).send(err);
+		}
+
+		return res.send(
+			data.replace('<div id="root"></div>', `<div id="root">${reactApp}</div>`)
+		);
+	});
 });
 
 app.listen(8080, () => {
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 
 import { Home } from "./pages/Home";
 import { Articles } from "./pages/Articles";
@@ -8,31 +8,29 @@ import { About } from "./pages/About";
 function App() {
 	return (
 		<>
-			<BrowserRouter>
-				<h1>Server-Side Rendering Example</h1>
-				<ul>
-					<li>
-						<Link to='/'>Home</Link>
-					</li>
-					<li>
-						<Link to='/about'>About</Link>
-					</li>
-					<li>
-						<Link to='/articles'>Articles</Link>
-					</li>
-				</ul>
-				<Switch>
-					<Route path='/' exact>
-						<Home />
-					</Route>
-					<Route path='/about' exact>
-						<About />
-					</Route>
-					<Route path='/articles' exact>
-						<Articles />
-					</Route>
-				</Switch>
-			</BrowserRouter>
+			<h1>Server-Side Rendering Example</h1>
+			<ul>
+				<li>
+					<Link to='/'>Home</Link>
+				</li>
+				<li>
+					<Link to='/about'>About</Link>
+				</li>
+				<li>
+					<Link to='/articles'>Articles</Link>
+				</li>
+			</ul>
+			<Switch>
+				<Route path='/' exact>
+					<Home />
+				</Route>
+				<Route path='/about' exact>
+					<About />
+				</Route>
+				<Route path='/articles' exact>
+					<Articles />
+				</Route>
+			</Switch>
 		</>
 	);
 }
